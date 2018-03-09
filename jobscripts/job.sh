72d6c15be494660953a5f03d1a9978b278862c46
Bac Nguyen Cong <bac.nguyencong@ugent.be>
2018-03-09 17:22:26 +0100

    Finish codes

---
+++ jobscripts/job.sh
@@ -0,0 +1,14 @@
+# ---- job script ---- #
+
+# activate python environment
+export CUDA_VISIBLE_DEVICES=1
+source activate pytorch-gpu
+
+# Move to src directory
+cd ..
+
+# run main script
+# The message variable is not used for now (will be used to update spreadsheet later)
+# Variable order: message im_size architecture epochs
+
+python main.py -img_size 512  resnet18 -epochs 50 -lr_patience 5 -early_stop 10 -lr 0.0001 --pretrained
