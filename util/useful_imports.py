956313ceeb37f64b2dfba70f399f4c2b9300601b
Bac Nguyen Cong <bac.nguyencong@ugent.be>
2018-03-19 16:32:15 +0100

    Updated

--- util/useful_imports.py
+++ util/useful_imports.py
@@ -14,4 +14,41 @@ from torchvision.datasets import ImageFolder
 from torchvision.transforms import ToTensor
 import util.utils as ut
 
-ROOT_DIR = './data/Reference pictures'
+from tqdm import tqdm
+import torch.nn as nn
+from model import CNNs
+from torch.autograd import Variable
+
+
+TEST_DIR = './data/test'
+TRAIN_DIR = './data/train'
+VALID_DIR = './data/valid'
+OUTPUT_FILE = './output/log.txt'
+OUTPUT_WEIGHT_PATH = './output/'
+GPU_AVAIL = torch.cuda.is_available()
+
+
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                  std=[0.229, 0.224, 0.225])
+valid_trans = transforms.Compose([
+        transforms.Lambda(lambda x: ut.crop_img(x, 2000, 1400)),
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        normalize
+])
+dset_valid = ImageFolder(root='data/valid/', transform=valid_trans)
+valid = transforms.Compose([
+        transforms.Lambda(lambda x: ut.crop_img(x, 2000, 1400)),
+        transforms.Resize((224, 224))
+])
+dsetreal = ImageFolder(root='data/valid/', transform=valid)
+
+
+data = ImageFolder(root=TRAIN_DIR,
+                   transform=
+                       transforms.Compose([
+                           transforms.Lambda(lambda x: ut.crop_img(x, 2000, 1400)),
+                           transforms.Resize((224, 224)),
+                           transforms.RandomHorizontalFlip(),
+                           transforms.RandomRotation(45,PIL.Image.BILINEAR)])
+                          )
